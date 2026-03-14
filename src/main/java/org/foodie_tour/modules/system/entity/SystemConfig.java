feb8ea6a173430cfcbfe5ee6452ee4d66a683f55
Hưng Trương <hungtqse182075@fpt.edu.vn>
2026-03-14 23:43:05 +0700

    feat: cancel tour

---
+++ src/main/java/org/foodie_tour/modules/system/entity/SystemConfig.java
@@ -0,0 +1,22 @@
+package org.foodie_tour.modules.system.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name = "system_configs")
+@AllArgsConstructor
+@NoArgsConstructor
+public class SystemConfig {
+
+    @Id
+    private String configKey;
+    private String configValue;
+    private String description;
+}
